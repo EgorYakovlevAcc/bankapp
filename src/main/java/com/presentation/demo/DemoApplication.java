39ab528cec5ca416d59b626d24b7423771291f14
Maxkile <pavel.sivashchenko@mail.ru>
2020-02-16 21:24:55 +0300

    Telegram bot support.

--- src/main/java/com/presentation/demo/DemoApplication.java
+++ src/main/java/com/presentation/demo/DemoApplication.java
@@ -2,11 +2,12 @@ package com.presentation.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
-
     public static void main(String[] args) {
+        ApiContextInitializer.init();
         SpringApplication.run(DemoApplication.class, args);
     }
 
--- src/main/java/com/presentation/demo/bot/config/TelegramBotConfig.java
+++ src/main/java/com/presentation/demo/bot/config/TelegramBotConfig.java
@@ -1,25 +1,31 @@
-package com.presentation.demo.config;
+package com.presentation.demo.bot.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 
 import javax.validation.constraints.Pattern;
 
 @Configuration
-@ConfigurationProperties(prefix = "telegram.bot")
+@ConfigurationProperties(value = "telegram.bot")
 public class TelegramBotConfig {
+
+    @Bean
+    public TelegramBotsApi getTelegramBotsApi(){
+        return new TelegramBotsApi();
+    }
 
     private String token;
 
-    @Pattern(regexp = "bot$")
     private String name;
 
     public String getToken() {
         return token;
     }
 
-    public void setToken(String auth_token) {
-        this.token = auth_token;
+    public void setToken(String token) {
+        this.token = token;
     }
 
     public String getName() {
